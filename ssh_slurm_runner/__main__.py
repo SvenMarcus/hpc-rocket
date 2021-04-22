5d8efd21b4856591dcc7f90ca7730ed16f18d6b6
Sven Marcus <s.marcus@outlook.de>
2021-04-22 08:40:32 +0200

    Added init to to watcher. Force exit in main on KeyboardInterrupt

--- ssh_slurm_runner/__main__.py
+++ ssh_slurm_runner/__main__.py
@@ -3,7 +3,6 @@ import signal
 from ssh_slurm_runner.watcher.jobwatcher import JobWatcher
 import sys
 
-from rich.console import RenderGroup
 from rich.live import Live
 from rich.spinner import Spinner
 
@@ -22,19 +21,21 @@ client.connect(cli_args.user, keyfile=cli_args.keyfile)
 runner = SlurmRunner(client)
 jobid = runner.sbatch(cli_args.jobfile)
 
+watcher_ctx = {"job": None}
+watcher = JobWatcher(runner)
+
 
 def handle_sigint(live: Live):
     try:
         print(f"Canceling job {jobid}")
         runner.scancel(jobid)
         job = runner.poll_status(jobid)
+        watcher.stop()
         live.update(make_table(job))
     except SlurmError as err:
         print(err.args)
+    sys.exit(1)
 
-
-watcher_ctx = {"job": None}
-watcher = JobWatcher(runner)
 
 with Live(Spinner("bouncingBar", "Launching job"), refresh_per_second=8) as live:
     signal.signal(signal.SIGINT, lambda _, __: handle_sigint(live))
@@ -48,6 +49,7 @@ with Live(Spinner("bouncingBar", "Launching job"), refresh_per_second=8) as live
     while not watcher.is_done():
         continue
 
+    watcher.stop()
     client.disconnect()
 
     job = watcher_ctx["job"]
