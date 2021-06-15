e035e1f28e9591c6ababa64c8e424e07a9272ade
Sven Marcus <s.marcus@outlook.de>
2021-06-15 13:13:00 +0000

    More work on SSHFileSystem

--- ssh_slurm_runner/sshfilesystem.py
+++ ssh_slurm_runner/sshfilesystem.py
@@ -1,20 +1,79 @@
+from abc import ABC, abstractmethod
+import fs.base
 import fs.copy as fscp
 import fs.sshfs as sshfs
 from ssh_slurm_runner.filesystem import Filesystem
 
 
-class SSHFilesystem(Filesystem):
-    
+class PyFilesystemBased(Filesystem, ABC):
+    """
+    Abstract base class for Filesystems based on PyFilesystem2
+    """
+
+    @property
+    @abstractmethod
+    def internal_fs(self) -> fs.base.FS:
+        """Returns the internally used PyFilesystem
+
+        Returns:
+            fs.base.FS: The internal PyFilesystem
+        """
+        pass
+
+
+class SSHFilesystem(PyFilesystemBased):
+    """
+    A PyFilesystem2 based Filesystem that connects to a remote machine via SSH
+    """
+
     def __init__(self, user: str, host: str, private_key: str) -> None:
-        self.fs = sshfs.SSHFS(host, user=user, pkey=private_key)
+        """
+        Args:
+            user (str): The user on the remote machine
+            host (str): The address of the remote machine
+            private_key (str): The user's private SSH key. Needed for authentication.
+        """
+        self._internal_fs = sshfs.SSHFS(host, user=user, pkey=private_key)
+
+    @property
+    def internal_fs(self) -> fs.base.FS:
+        return self._internal_fs
 
     def copy(self, source: str, target: str, filesystem: 'Filesystem' = None) -> None:
+        self._raise_if_source_does_not_exist(source)
+        self._raise_if_target_exists(target, filesystem)
+
         if filesystem:
-            fscp.copy_file(self.fs, source, filesystem.fs, target)
+            self._try_copy_to_filesystem(source, target, filesystem)
             return
 
-        self.fs.copy(source, target)
-        
+        self._try_copy(source, target)
+
+    def _raise_if_source_does_not_exist(self, source):
+        if not self.exists(source):
+            raise FileNotFoundError(source)
+
+    def _raise_if_target_exists(self, target, filesystem):
+        target_filesystem = filesystem or self
+        if target_filesystem.exists(target):
+            raise FileExistsError(target)
 
     def delete(self, path: str) -> None:
-        pass
+        if not self.exists(path):
+            raise FileNotFoundError(path)
+
+        self.internal_fs.remove(path)
+
+    def exists(self, path: str) -> None:
+        return self.internal_fs.exists(path)
+
+    def _try_copy_to_filesystem(self, source, target, filesystem):
+        if not isinstance(filesystem, PyFilesystemBased):
+            raise RuntimeError(
+                "SSHFilesystem currently only works with PyFilesystem2 based Filesystems")
+
+        fscp.copy_file(self.internal_fs, source,
+                       filesystem.internal_fs, target)
+
+    def _try_copy(self, source, target):
+        self.internal_fs.copy(source, target)
--- test/test_sshfilesystem.py
+++ test/test_sshfilesystem.py
@@ -1,19 +1,46 @@
-from ssh_slurm_runner.filesystem import Filesystem
+import fs.base
 import pytest
-from ssh_slurm_runner.sshfilesystem import SSHFilesystem
+from ssh_slurm_runner.filesystem import Filesystem
+from ssh_slurm_runner.sshfilesystem import SSHFilesystem, PyFilesystemBased
 from unittest.mock import MagicMock, patch
 
 
-class MockFilesystem(Filesystem):
+class MockFilesystem(PyFilesystemBased):
 
     def __init__(self) -> None:
-        self.fs = MagicMock(spec="fs.base.FS").return_value
+        self._internal_fs = MagicMock(spec="fs.base.FS").return_value
+        self.existing_files = set()
+
+    def internal_fs(self) -> fs.base.FS:
+        return self._internal_fs
 
     def copy(self, source: str, target: str, filesystem: 'Filesystem') -> None:
         pass
 
     def delete(self, path: str) -> None:
         pass
+
+    def exists(self, path: str) -> None:
+        return path in self.existing_files
+
+
+class NonPyFilesystemBasedFilesystem(Filesystem):
+
+    def __init__(self) -> None:
+        pass
+
+    def copy(self, source: str, target: str, filesystem: 'Filesystem') -> None:
+        pass
+
+    def delete(self, path: str) -> None:
+        pass
+
+    def exists(self, path: str) -> None:
+        pass
+
+
+SOURCE = "~/file.txt"
+TARGET = "~/another/folder/copy.txt"
 
 
 @pytest.fixture
@@ -22,7 +49,10 @@ def sshfs_type_mock():
     patcher1 = patch("fs.sshfs.sshfs.SSHFS")
     patcher2 = patch("fs.sshfs.SSHFS")
     patcher1.start()
-    yield patcher2.start()
+    mock = patcher2.start()
+    mock.return_value.configure_mock(exists=exists_with_files(SOURCE))
+
+    yield mock
 
     patcher1.stop()
     patcher2.stop()
@@ -46,19 +76,77 @@ def test__given_ssh_client__when_creating_new_instance__should_create_sshfs_with
 def test__when_copying_file__should_call_copy_on_sshfs(sshfs_type_mock):
     sut = SSHFilesystem('user', 'host', 'privatekey')
 
-    sut.copy("~/file.txt", "~/another/folder/copy.txt")
+    sut.copy(SOURCE, TARGET)
 
     sshfs_mock = sshfs_type_mock.return_value
-    sshfs_mock.copy.assert_called_with(
-        "~/file.txt", "~/another/folder/copy.txt")
+    sshfs_mock.copy.assert_called_with(SOURCE, TARGET)
 
 
 def test__when_copying_file_to_other_filesystem__should_call_copy_file(sshfs_type_mock, copy_file):
     fs_mock = MockFilesystem()
     sut = SSHFilesystem('user', 'host', 'privatekey')
 
-    sut.copy("~/file.txt", "~/another/folder/copy.txt", filesystem=fs_mock)
+    sut.copy(SOURCE, TARGET, filesystem=fs_mock)
 
     sshfs_mock = sshfs_type_mock.return_value
     copy_file.assert_called_with(
-        sshfs_mock, "~/file.txt", fs_mock.fs, "~/another/folder/copy.txt")
+        sshfs_mock, SOURCE, fs_mock.internal_fs, TARGET)
+
+
+def test__when_copying__but_source_does_not_exist__should_raise_file_not_found_error(sshfs_type_mock):
+    sshfs_type_mock.return_value.configure_mock(exists=exists_with_files())
+    sut = SSHFilesystem('user', 'host', 'privatekey')
+
+    with pytest.raises(FileNotFoundError):
+        sut.copy(SOURCE, TARGET)
+
+
+def test__when_copying__but_file_exists__should_raise_file_exists_error(sshfs_type_mock):
+    sshfs_type_mock.return_value.configure_mock(
+        exists=exists_with_files(SOURCE, TARGET))
+
+    sut = SSHFilesystem('user', 'host', 'privatekey')
+
+    with pytest.raises(FileExistsError):
+        sut.copy(SOURCE, TARGET)
+
+
+def test__when_copying_to_other_filesystem__but_file_exists__should_raise_file_exists_error(sshfs_type_mock, copy_file):
+    fs_mock = MockFilesystem()
+    fs_mock.existing_files.add(TARGET)
+    sut = SSHFilesystem('user', 'host', 'privatekey')
+
+    with pytest.raises(FileExistsError):
+        sut.copy(SOURCE, TARGET, filesystem=fs_mock)
+
+
+def test__when_copying_to_non_pyfilesystem__should_raise_runtime_error(sshfs_type_mock):
+    fs_mock = NonPyFilesystemBasedFilesystem()
+    sut = SSHFilesystem('user', 'host', 'privatekey')
+
+    with pytest.raises(RuntimeError):
+        sut.copy(SOURCE, TARGET, filesystem=fs_mock)
+
+
+def test__when_deleting_file__should_call_sshfs_delete(sshfs_type_mock):
+    sut = SSHFilesystem('user', 'host', 'privatekey')
+
+    sut.delete(SOURCE)
+
+    sshfs_mock = sshfs_type_mock.return_value
+    sshfs_mock.remove.assert_called_with(SOURCE)
+
+
+def test__when_deleting_file_but_does_not_exist__should_raise_file_not_found_error(sshfs_type_mock):
+    sshfs_type_mock.return_value.configure_mock(exists=exists_with_files())
+    sut = SSHFilesystem('user', 'host', 'privatekey')
+
+    with pytest.raises(FileNotFoundError):
+        sut.delete(SOURCE)
+
+
+def exists_with_files(*args):
+    def exists(path: str):
+        return path in args
+
+    return exists
