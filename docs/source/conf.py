883139f7483d8acb1d53e6511d7338125d555559
Sven Marcus <s.marcus@outlook.de>
2022-12-13 09:45:14 +0100

    Bump version in sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Sven Marcus"
 author = "Sven Marcus"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
